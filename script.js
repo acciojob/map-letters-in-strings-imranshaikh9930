4517ad685a148215014fa59059b8a349240f62a0
imran <87297004+imranshaikh9930@users.noreply.github.com>
2025-02-05 13:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+function mapLetters(string){
+
+	let result = {};
+	for(let i = 0 ; i < string.length;i++){
+		const letter = string[i];
+
+		if(!result[letter]){
+			result[letter] = [];
+		}
+
+		result[letter].push(i);
+	}
+	return result;
+}
